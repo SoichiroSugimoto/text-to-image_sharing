84dc5c3141d929c2d094f1e40bb8e2cc9b74284f
SoichiroSugimoto <fcsoichi1998@gmail.com>
2023-01-15 00:44:33 +0900

    insert post belongs to user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,13 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+	  @post = @user.posts.build(text: params[:text], image: params[:image])
+	  @post.save
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'posts/index'
   root 'static_pages#home'
   resources :users
+  resources :posts
 end
