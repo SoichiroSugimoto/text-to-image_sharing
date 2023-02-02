f05e0455acf22c7d99a30159562c7b1c6a5b9d44
SoichiroSugimoto <fcsoichi1998@gmail.com>
2023-02-03 01:54:03 +0900

    setup faraday connection

--- lib/util/http_request.rb
+++ lib/util/http_request.rb
@@ -1,2 +1,11 @@
 module HttpRequest
+  def setup_connection
+    uri = "http://[適当なURI]/"
+    req = Faraday::Connection.new(url: uri) do |conn|
+      conn.adapter Faraday.default_adapter
+      conn.request :url_encoded
+      conn.response :logger # ログを出す
+      conn.headers['Content-Type'] = 'application/json' # ヘッダー指定
+    end
+  end
 end
